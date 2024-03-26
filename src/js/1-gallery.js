d61fe36b8781a5fb7313d2ca1d224643595379cd
Iryna Serhiienko <ivus0888@gmail.com>
2024-03-26 18:36:14 +0200

    home-work-js-09

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,31 @@
-console.log('Gallery');
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { images } from './gallery-images.js';
+
+const galleryList = document.querySelector('ul.gallery');
+const imagesMarkup = images
+  .map(
+    ({ preview, original, description }) => `
+            <li class="gallery-item">
+                <a class="large-img" href="${original}" >
+                <img
+                class="gallery-image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+        </li>`
+  )
+  .join('');
+
+galleryList.insertAdjacentHTML('beforeend', imagesMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
+
+console.log(SimpleLightbox);
